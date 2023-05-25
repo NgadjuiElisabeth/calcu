70da1d517a7057c690bda9a472dd23e9b4767272
AkaSaru <akasaru0@protonmail.com>
2023-05-25 17:35:27 +0200

    Update main.dart
    Rajoute de la fonction de réception des données.

--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,35 @@ void main() {
     url: 'https://ptundmtrsdaxtqetgbsk.supabase.co',
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB0dW5kbXRyc2RheHRxZXRnYnNrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQyNTMxODgsImV4cCI6MTk5OTgyOTE4OH0.0o4wa3-r7gZoFb6DYywUQ3IoEEtBvGbmdjR8ftq-FEA',
   );
-  sendDataToSupabase("2+50","52");
   runApp(CalculatriceAPP());
 }
 
+//C'est la fonction pour récupérer les données dans la supabase
+Future<List<Map<String, dynamic>>> getDataToSupabase() async {
+    final supabase = Supabase.instance.client;
 
+    // Nom de la table contenant les données à récupérer
+    String tableName = 'history';
+
+    final response = await supabase.from(tableName).select('*').order('created_at', ascending: false).limit(5).execute();
+    List<Map<String, dynamic>> extractedData = [];
+    for (final entry in response.data) {
+      Map<String, dynamic> extractedValues = {
+        'calcul': entry['calcul'],
+        'result': entry['result'],
+      };
+      extractedData.add(extractedValues);
+    }
+    return extractedData;
+
+}
+
+//c'est la fonction pour envoyer les données dans la supabase
 void sendDataToSupabase(String calcul,String result) async {
   final supabase = Supabase.instance.client;
   Random random = Random();
   int id = random.nextInt(100000);
+  print(id);
   // Données à envoyer
   Map<String, dynamic> data = {
     'id': id,
@@ -27,6 +47,7 @@ void sendDataToSupabase(String calcul,String result) async {
 
   // Table dans laquelle les données seront insérées
   String tableName = 'history';
+  print("la");
   final response = await supabase.from(tableName).insert([data]).execute();
 }
 
