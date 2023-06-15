6d3bf5792106fbd7469c9bc97a7588fe62fabe29
AkaSaru <akasaru0@protonmail.com>
2023-06-15 18:46:51 +0200

    Update CalculariceHistorique.dart

--- lib/Calculatrice/CalculariceHistorique.dart
+++ lib/Calculatrice/CalculariceHistorique.dart
@@ -1,7 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-class HistoriqueCalcu extends StatelessWidget {
+class HistoriqueCalcu extends StatefulWidget {
   const HistoriqueCalcu({Key? key}) : super(key: key);
+
+  @override
+  _HistoriqueCalcuState createState() => _HistoriqueCalcuState();
+}
+
+class _HistoriqueCalcuState extends State<HistoriqueCalcu> {
+  Future<List<Map<String, dynamic>>> getDataToSupabase() async {
+    final supabase = Supabase.instance.client;
+
+    // Nom de la table contenant les données à récupérer
+    String tableName = 'history';
+
+    final response = await supabase
+        .from(tableName)
+        .select('*')
+        .order('created_at', ascending: false)
+        .limit(5)
+        .execute();
+    List<Map<String, dynamic>> extractedData = [];
+    for (final entry in response.data) {
+      Map<String, dynamic> extractedValues = {
+        'calcul': entry['calcul'],
+        'result': entry['result'],
+      };
+      extractedData.add(extractedValues);
+    }
+    return extractedData;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,9 +48,33 @@ class HistoriqueCalcu extends StatelessWidget {
               style: TextStyle(fontSize: 24),
             ),
             SizedBox(height: 20),
+            FutureBuilder<List<Map<String, dynamic>>>(
+              future: getDataToSupabase(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<List<Map<String, dynamic>>> snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return CircularProgressIndicator();
+                } else if (snapshot.hasError) {
+                  return Text('Erreur : ${snapshot.error}');
+                } else {
+                  List<Map<String, dynamic>> data = snapshot.data!;
+                  return ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: data.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return ListTile(
+                        title: Text(data[index]['calcul']),
+                        subtitle: Text(data[index]['result']),
+                      );
+                    },
+                  );
+                }
+              },
+            ),
+            SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
-                Navigator.pushReplacementNamed(context, '/Accueil'); // Navigation vers la page d'accueil
+                Navigator.pushReplacementNamed(context, '/Accueil');
               },
               child: Text("Retour à l'accueil"),
             ),
